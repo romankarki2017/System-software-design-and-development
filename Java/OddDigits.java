f721b9213a4e8b1e9c3af4484854dd452f05770c
Roman Karki <romankarki2017@gmail.com>
2026-03-31 08:33:04 +0545

    number of odd digits calculator

---
+++ Java/OddDigits.java
@@ -0,0 +1,23 @@
+//Write the program to count the number of odd digits in a give number.
+//Ex: if number n=4572 no of odd digits=2
+
+import java.util.Scanner;
+
+public class OddDigits {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner (System.in); 
+        System.out.println("Enter a number: ");
+        n=sc.nextInt();
+        int count =0;
+        while (n>0) {
+            int singleDigit= n%10;
+            if(singleDigit%2 != 0){
+                count++;
+            }
+            n=n/10;
+        }
+        System.out.println("No. of odd digits: " +count);
+    }
+    
+}
