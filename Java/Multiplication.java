3f74f235102eb7e61220731b2caa67666ee91b4e
Roman Karki <romankarki2017@gmail.com>
2026-03-31 08:31:05 +0545

    multiplication table calculator made successfully

---
+++ Java/Multiplication.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] agrs){
+        int n;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        n=sc.nextInt();
+
+        System.out.println();
+        for(int i=1; i<=10; i++){
+            System.out.println( n + "x" + i + " = " + (n*i));
+        }
+        
+    }
+
+
+}
