751302a5d5959d48af46e4249d4190e63a5a5d69
Roman Karki <romankarki2017@gmail.com>
2026-04-03 07:51:40 +0545

    calculate the gpa and give the average

---
+++ Java/Gpa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Gpa {
+    public static void main(String[] args){
+        float[] gpa = new float[5];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<=4; i++){
+            System.out.print("Enter the gpa of student" + (i+1) + ": ");
+            gpa[i]= sc.nextFloat();
+
+        }
+        float sum=0; 
+        // sum=gpa[0]+gpa[1]+gpa[2]+gpa[3]+gpa[4]; 
+        for(int i=0; i<=4; i++){
+            sum=sum+gpa[i];
+        } 
+        float avg=sum/5;
+        System.out.println("Average gpa = " +avg);
+    }
+}
