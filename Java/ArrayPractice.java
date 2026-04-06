e90760c7f094abacc3c93b4459eef2f471ac84ed
Roman Karki <romankarki2017@gmail.com>
2026-04-06 07:40:47 +0545

    practiced the array function

---
+++ Java/ArrayPractice.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ArrayPractice {
+    public static void main(String[] args){
+        int rollno[];
+        rollno=new int[10];
+        rollno[0]=23;
+    }
+}
